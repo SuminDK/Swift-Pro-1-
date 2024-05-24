1f703262f09dee33a1e1b5b4a0f7486cf3b6b120
Sumin Kim <411sumin@gmail.com>
2024-05-24 21:16:16 +0900

    2024-05-24: complete delete function

--- ToDoListProject/VM/ToDoListViewVM.swift
+++ ToDoListProject/VM/ToDoListViewVM.swift
@@ -5,6 +5,7 @@
 //  Created by Diana Kim on 5/20/24.
 //
 
+import FirebaseFirestore
 import Foundation
 
 // VM for list of items
@@ -13,10 +14,22 @@ import Foundation
 class ToDoListViewViewModel: ObservableObject{
     
     @Published var showingNewItemView = false
+    private let userId: String
     
-    init () {}
+    init (userId: String) {
+        self.userId = userId
+    }
     
+    /// Delete todo list item
+    /// - Parameter id: item id to delete
     func delete(id: String){
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
         
     }
 }
