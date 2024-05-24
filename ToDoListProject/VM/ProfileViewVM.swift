d9003ff7ad375dee2e1d7c326ef3bc954e5b1881
Sumin Kim <411sumin@gmail.com>
2024-05-24 21:26:56 +0900

    2024-05-24: check function completed

--- ToDoListProject/VM/ProfileViewVM.swift
+++ ToDoListProject/VM/ProfileViewVM.swift
@@ -5,12 +5,27 @@
 //  Created by Diana Kim on 5/20/24.
 //
 
+import FirebaseFirestore
+import FirebaseAuth
 import Foundation
 
 class ProfilViewViewModel: ObservableObject{
     init () {}
     
     func toggleIsDone(item: ToDoListItem){
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else{
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
         
     }
 }
